488e1a16f5e65be5954f4ad5f2ca0b48e18b9235
Ohimoiza1205 <163584196+Ohimoiza1205@users.noreply.github.com>
2024-03-15 17:44:00 -0500

    Update 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-"""Task 2's module"""
+"""Task 2's module
+"""
+
 
 def floor(a: float) -> int:
-    """Computes the floor of a floating-point number"""
+    """Computes the floor of a floating-point number
+    """
+
+    
     return int(a)
