720ceab0835d8544cc45a0cc2e1d3752210b1802
Ohimoiza1205 <163584196+Ohimoiza1205@users.noreply.github.com>
2024-03-15 17:21:33 -0500

    Update 4-main.py

--- 0x00-python_variable_annotations/4-main.py
+++ 0x00-python_variable_annotations/4-main.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-"""assign the import statements"""
-
 a = __import__('4-define_variables').a
 pi = __import__('4-define_variables').pi
 i_understand_annotations = __import__('4-define_variables').i_understand_annotations
 school = __import__('4-define_variables').school
-
-"""print the main function"""
 
 print("a is a {} with a value of {}".format(type(a), a))
 print("pi is a {} with a value of {}".format(type(pi), pi))
