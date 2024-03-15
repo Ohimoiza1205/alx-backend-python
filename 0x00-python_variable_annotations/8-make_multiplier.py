7ca309681cf4907f27c268165cce4088e4465af1
Ohimoiza1205 <163584196+Ohimoiza1205@users.noreply.github.com>
2024-03-15 17:49:59 -0500

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-"""Task 8's module
+"""Write a type-annotated function make_multiplier that takes
+a float multiplier as argument and returns a function that
+multiplies a float by multiplier.
 """
 
 
-from typing import Callable
-def make_multiplier(multiplier: float) -> Callable[[float], float]:
-"""Creates a multiplier function
-"""
+import typing
 
 
-    return lambda x: x * multiplier
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier"""
+    def float_multiply(x: float) -> float:
+        return multiplier * x
+
+    return float_multiply
