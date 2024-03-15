d5449eda748c3e76966fc91221ae3468aa864c29
Ohimoiza1205 <163584196+Ohimoiza1205@users.noreply.github.com>
2024-03-15 17:25:53 -0500

    Update 8-main.py

--- 0x00-python_variable_annotations/8-main.py
+++ 0x00-python_variable_annotations/8-main.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
-"""main code for multiplier"""
-
 make_multiplier = __import__('8-make_multiplier').make_multiplier
 print(make_multiplier.__annotations__)
-
-"""main code for fun"""
 fun = make_multiplier(2.22)
 print("{}".format(fun(2.22)))
